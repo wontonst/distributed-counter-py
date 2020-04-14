cf2e670cee1f599e6cdf8e87d025e66f8941590a
Roy Zheng <royz@23andme.com>
2020-04-14 11:14:43 -0700

    Update README with long description

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='distributed_counter',
     version='0.0.2',
     description='distributed increment/decrement counter leveraging dynamodb',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wontonst/distributed-counter-py',
     author='Roy Zheng',
     packages=find_packages(exclude=['tests','docs']),
     install_requires=['boto3'],
